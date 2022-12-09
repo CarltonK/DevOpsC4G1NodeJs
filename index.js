a9163d6cbc30d193cf249de742bf65c7cbe54db2
Mark Carlton <mcarlton33@gmail.com>
2022-12-09 09:25:40 +0300

    initial application

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+// Create an application
+const app = express();
+const port = 3000;
+
+// Create my first request handler
+app.get('/', (req, res) => {
+    res.status(200).send({ message: true });
+});
+
+app.post('/', (req, res) => {
+    res.status(200).send({ message: true });
+});
+
+app.listen(port, () => {
+    console.log('The server is listening for requests');
+});
